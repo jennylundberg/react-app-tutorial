a216f0ba8a75e5502505d647ca94a2f26681ee80
Jenny Lundberg <elsa.jenny.lundberg00@hotmail.com>
2023-07-04 14:04:07 +0200

    started sending in list of object to tweet component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import Tweet from "./Tweet";
 
 function App() {
 
+  const [users, setUsers] = useState([
+    {name: "Jenny", message: "This is a random tweet", likes: 14},
+    {name: "Alwin", message: "Carpe Diem", likes: 21},
+    {name: "Axel", message: "Bananaaaas!", likes: 17},
+    {name:"Lina", message:"I Like Trains", likes: 23}
+  ])
+
   return (
     <div className="app">
-      <Tweet name="Jenny" message="This is a random tweet"></Tweet>
-      <Tweet name="Alwin" message="Carpe Diem"></Tweet>
-      <Tweet name="Axel" message="Bananaaaas!"></Tweet>
-      <Tweet name="Lina" message="I Like Trains"></Tweet>
+      {users.map(user => (
+          <Tweet name={user.name} message={user.message} likes={user.likes}></Tweet>
+      ))}
     </div>
   );
 }
--- src/Tweet.js
+++ src/Tweet.js
@@ -6,7 +6,7 @@ function Tweet(props) {
     <div className="tweet">
       <h3>{props.name}</h3>
       <p>{props.message}</p>
-      <h3>Number of likes</h3>
+      <h3>Number of likes: {props.likes}</h3>
     </div>
   );
 }
